ec03be92dc3940a0108f047097b7813443912363
karthikeya palada <karthikeya.palada@gmail.com>
2024-06-10 03:26:33 -0400

    commited on 10-06-2006

---
+++ 020.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{ int km,m;
+   km=m=0;
+
+   printf("enter the distance travelled by bus in kms :");
+   scanf("%d",&km);
+
+   m=km*1000;
+
+   printf("the distance in meters is :- %d m",m);
+  }
