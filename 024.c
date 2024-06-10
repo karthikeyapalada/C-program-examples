21eac3632e9c059635c5edf983063ef8e9904ae0
karthikeya palada <karthikeya.palada@gmail.com>
2024-06-10 03:30:14 -0400

    024.c

---
+++ 024.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main()
+{ int a,b,result;
+  a=b=result=0;
+
+  printf("enter the value of A :");
+  scanf("%d",&a);
+
+  printf("enter the value of B :");
+  scanf("%d",&b);
+
+  result=a*b;
+
+  printf("the product is :%d",result);
+}
