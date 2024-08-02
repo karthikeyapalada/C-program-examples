c856009f5afb2b1d50bff34f176699ff0181e0f5
karthikeya palada <karthikeya.palada@gmail.com>
2024-08-02 23:00:31 +0530

    Create 033.c

---
+++ 033.c
@@ -0,0 +1,21 @@
+ *\\C PROGRAM TO READ 3 PENS PRICE AND DISPLAY WHICH IS COSTLY.\\*
+#include <stdio.h>
+main()
+ {
+    int p1,p2,p3;
+     p1=p2=p3=0;
+     printf("enter price of reynolds :");
+     scanf("%d",&p1);
+     printf("enter price of montex : ");
+     scanf("%d",&p2);
+     printf("enter price of parker :");
+     scanf("%d",&p3);
+
+     if (p1>p2 && p1>p3)
+        printf("Reynolds is costliest");
+       else
+           if (p2>p1 && p2>p3)
+              printf("Montex is costliest");
+             else
+                printf("Parker is costliest");  
+ }
