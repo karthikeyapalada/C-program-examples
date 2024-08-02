ea45c6af270880d5ab37ab91f246fbf39d4062ba
karthikeya palada <karthikeya.palada@gmail.com>
2024-08-02 22:59:16 +0530

    Create 032.c

---
+++ 032.c
@@ -0,0 +1,16 @@
+\\C PROGRAM TO CHECK IF A GIVEN IS DIVISIBLE BY BOTH three and five.
+#include<stdio.h>
+main()
+ { int number;
+   printf("Enter a number: ");
+    scanf("%d", &number);
+
+    if (number % 3 == 0 && number % 5 == 0) 
+        {
+        printf("%d is divisible by both 3 and 5.\n", number);
+        }
+    else
+     {
+        printf("%d is not divisible by both 3 and 5.\n", number);
+     }
+}
